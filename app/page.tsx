6581afad79e35c0724328690bcc62c82899e849f
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-14 10:54:18 +0630

    feat: create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import LoginButton from "@/components/ui/auth/login-button";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button size="lg" variant="custom">
-      Click Me
-    </Button>
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-r from-sky-400 to to-blue-500">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
