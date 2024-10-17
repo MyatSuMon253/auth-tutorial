d81bbcf3eb7f4c41cf02af711440b4ac7045ecd4
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-17 19:28:09 +0630

    feat: configure next auth

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import authConfig from "./auth.config";
+import { db } from "./lib/db";
 
 export const {
-  handlers: { GET, POST }, auth
+  handlers: { GET, POST },
+  auth,
 } = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
-import { auth } from "./auth";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const isLoggedIn = !!req.auth;
   console.log("ROUTE: ", req.nextUrl);
+  console.log("IS LOGGEDIN", isLoggedIn);
 });
 
+// optionally don't invoke middleware on some paths
 export const config = {
-  matcher: ["/login"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
