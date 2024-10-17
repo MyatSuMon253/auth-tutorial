e1a7e3331b92ebee6e43e4d458b919f8ed29039e
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-17 22:08:42 +0630

    feat: add authorize function in auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,31 @@
+import bcrypt from "bcrypt";
 import type { NextAuthConfig } from "next-auth";
-import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { getUserByEmail } from "./data/user";
+import { LoginSchema } from "./schema";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateFields = LoginSchema.safeParse(credentials);
+
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+
+          return null;
+        }
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
