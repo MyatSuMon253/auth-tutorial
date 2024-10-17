db7fe060db170bf4536cae8454c74f782d6b42ca
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-17 21:29:12 +0630

    feat: define auth function

---
+++ routes.ts
@@ -0,0 +1,14 @@
+// these routes do not require authentication
+export const publicRoutes = ["/"];
+
+// these routes will redirect logged in users to /settings
+export const authRoutes = ["/login", "/register"];
+
+/*
+prefix for API authentication routes
+routes that start with this prefix are used for API authentication purposes
+*/
+export const apiAuthPrefix = "/api/auth";
+
+// default redirect path after logging in
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
