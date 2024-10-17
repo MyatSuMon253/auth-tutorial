a853cec9ce062ea3fa0d052e3862169e2908e26b
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-17 23:32:20 +0630

    fix: fix error in bcrypt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      use: "html-loader", // Or 'ignore-loader' if you want to ignore HTML files
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
