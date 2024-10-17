c3bc29727afeb98230ed057978e85ea7332109e2
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-17 10:10:46 +0630

    feat: add register functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schema";
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,6 +12,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields) {
     return { error: "Invalid Fields!" };
   }
+
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  const user = await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // send verification token email
 
   return { success: "Email sent!" };
 };
