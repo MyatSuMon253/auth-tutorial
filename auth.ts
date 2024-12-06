c6c3b62d18df490f25bbb83f0839e8dddb9d6bfb
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-12-06 09:45:15 +0630

    feat: add oauth credentials

--- auth.ts
+++ auth.ts
@@ -12,13 +12,13 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
-    async signIn({ user }) {
-      const existingUser = await getUserById(user?.id as string);
-      if (!existingUser || !existingUser?.emailVerified) {
-        return false;
-      }
-      return true;
-    },
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user?.id as string);
+    //   if (!existingUser || !existingUser?.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
     async session({ token, session }) {
       if (session.user && token.sub) {
         session.user.id = token.sub;
