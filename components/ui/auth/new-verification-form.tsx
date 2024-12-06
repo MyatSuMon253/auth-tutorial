bcc7b5690bb0c10387b5099e87055457bfdea58d
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-12-06 16:23:04 +0630

    feat: bind verify function in verification form and show error or success

--- components/ui/auth/new-verification-form.tsx
+++ components/ui/auth/new-verification-form.tsx
@@ -1,16 +1,34 @@
 "use client";
 
+import { newVerification } from "@/actions/new-verification";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { CircleLoader } from "react-spinners";
 import CardWrapper from "./card-wrapper";
 
 const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data?.success);
+        setError(data?.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -25,7 +43,9 @@ const NewVerificationForm = () => {
       showSocial={false}
     >
       <div className="w-full flex items-center justify-center">
-        <CircleLoader />
+        {!success && !error && <CircleLoader color="#3B82F6" />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
