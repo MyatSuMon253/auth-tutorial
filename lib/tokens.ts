228a707510a077fa9b64c1a6ab7160baf88e072e
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-12-08 08:22:41 +0630

    feat: generate password reset token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,29 @@
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    return await db.passwordResetToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -10,7 +33,7 @@ export const generateVerificationToken = async (email: string) => {
   if (existingToken) {
     return await db.verificationToken.delete({
       where: { id: existingToken.id },
-    }); 
+    });
   }
 
   const verificationToken = await db.verificationToken.create({
