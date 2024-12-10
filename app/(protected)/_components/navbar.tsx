43d4f59f13a89e20525fcaf130294b6dc2889e82
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-12-10 08:43:34 +0630

    feat: add user button and navbar

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
+import UserButton from "@/components/ui/auth/user-button";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -11,14 +11,20 @@ const Navbar = () => {
   return (
     <div className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
       <div className="flex gap-x-2">
-        <Button variant={pathname === '/server' ? 'default' : 'outline'} asChild>
+        <Button
+          variant={pathname === "/server" ? "default" : "outline"}
+          asChild
+        >
           <Link href="/server">Server</Link>
         </Button>
-        <Button variant={pathname === '/settings' ? 'default' : 'outline'} asChild>
+        <Button
+          variant={pathname === "/settings" ? "default" : "outline"}
+          asChild
+        >
           <Link href="/settings">Settings</Link>
         </Button>
       </div>
-      <p>User Button</p>
+      <UserButton />
     </div>
   );
 };
